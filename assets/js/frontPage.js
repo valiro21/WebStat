67daacdc85afb423036b4ef209d6421cbbf31dbd
neteduandrei <andrei.netedu2009@gmail.com>
2017-11-11 16:38:47 +0200

    Add basic navigation

---
+++ assets/js/frontPage.js
@@ -0,0 +1,7 @@
+var RenderStatisticsDrive = function() {
+    window.location.href = '../pages/realTimeData.html';
+};
+
+var RenderRealTimeData = function() {
+    window.location.href = '../pages/statisticsDrive.html';
+};
