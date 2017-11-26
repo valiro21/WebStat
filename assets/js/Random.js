d807c95d8487d1339921a84c50cd61fbf9a96873
Cosmin Pascaru <cosmin.pascaru2@gmail.com>
2017-11-26 23:24:10 +0200

    Refactor and generalize chart generation.

---
+++ assets/js/Random.js
@@ -0,0 +1,33 @@
+
+function getRandomInt(a, b) {
+    return a + Math.floor(Math.random() * (b - a));
+}
+
+function getRandomRgb() {
+    r = getRandomInt(0, 255);
+    g = getRandomInt(0, 255);
+    b = getRandomInt(0, 255);
+
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+function getRgbaFromRgbSingle(rgb, alpha) {
+    return rgb.replace('rgb', 'rgba').replace(')', ', ' + alpha + ')');
+}
+
+function getRgbaFromRgbArray(rgb, alpha) {
+    ans = [];
+    rgb.forEach( function (col) {
+        ans.push(getRgbaFromRgbSingle(col, alpha))
+    });
+
+    return ans;
+}
+
+function getRandomColors(cnt) {
+    colors = [];
+    for (i = 0; i < cnt; ++i) {
+        colors.push(getRandomRgb());
+    }
+    return colors;
+}
